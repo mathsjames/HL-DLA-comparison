72f237f18bbe165d43ecaac03e1a3844cbf85bd6
mathsjames <james.bell1@hotmail.com>
2018-12-06 22:21:38 +0000

    added directory construction to everything

--- everything.sh
+++ everything.sh
@@ -1,6 +1,10 @@
 # compile all code
 ./getcode
 
+# make necessary directories
+mkdir asizes
+mkdir location
+
 # generate actual size data for circular particle for later monte carlo using first 10 seeds
 ./runHL 1 10 $cores 1 2 1
 # take only 1 in every 10 samples and order them
