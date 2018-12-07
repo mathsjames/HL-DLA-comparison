da3ed42477a887e6efe886c24a7bd4b0a470c45b
James Bell <jbell@posteo.net>
2018-12-07 17:29:11 +0000

    Added empty directories for data to the repo

--- everything.sh
+++ everything.sh
@@ -1,13 +1,6 @@
 # compile all code
 ./getcode
 
-# make necessary directories
-mkdir asizes
-mkdir location
-mkdir edf
-mkdir distarray
-mkdir energies
-
 # generate actual size data for circular particle for later monte carlo using first 10 seeds
 ./runHL.sh 1 10 $cores 1 2 1
 # take only 1 in every 10 samples and order them
