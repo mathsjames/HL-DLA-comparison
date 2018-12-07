0d963aede443ef5a8d2fd4dc1225a65206e2a724
mathsjames <james.bell1@hotmail.com>
2018-12-07 00:15:05 +0000

    renamed more and deleted more rubbish

--- getcode.sh
+++ getcode.sh
@@ -2,10 +2,10 @@ cd src/HL/
 make
 mv ClusterHL ../../
 cd ../../
-gcc -o asizecompacter src/other/asizecompacter.c
-g++ -O3 -o Dimension src/other/dimension.cpp
-g++ -O3 -o Cluster src/DLA/Cluster.cpp
-g++ -o imager src/imagedraw/example.cpp
-gcc -o DistArray src/other/builddistarray.c
-gcc -o Energies src/other/permutationtest.c
+gcc -o ASizeCompacter src/other/asizecompacter.c
+g++ -O3 -o EDF src/other/edf.cpp
+g++ -O3 -o ClusterDLA src/DLA/clusterDLA.cpp
+g++ -o Imager src/imagedraw/imager.cpp
+gcc -o DistArray src/other/distarray.c
+gcc -o Energies src/other/energies.c
 
