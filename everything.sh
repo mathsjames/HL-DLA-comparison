590a54c28c1856fee276a101a98c9bc59752f744
James Bell <jbell@posteo.net>
2018-12-07 17:26:50 +0000

    corrected name of runHL to runHL.sh

--- everything.sh
+++ everything.sh
@@ -9,6 +9,6 @@ mkdir distarray
 mkdir energies
 
 # generate actual size data for circular particle for later monte carlo using first 10 seeds
-./runHL 1 10 $cores 1 2 1
+./runHL.sh 1 10 $cores 1 2 1
 # take only 1 in every 10 samples and order them
-./asizecompacter
+./ASizeCompacter
