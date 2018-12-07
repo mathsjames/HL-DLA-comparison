7987cc5f08af9157904b7e3b68e11e0f09a98dec
mathsjames <james.bell1@hotmail.com>
2018-12-07 11:58:49 +0000

    timing.sh now functions properly

--- timing.sh
+++ timing.sh
@@ -1,7 +1,7 @@
 mkdir times
 size=$1
-time ./ClusterHL -S 1 -r 1 -p 2 -g $size &> times/HL
-time ./ClusterHL -S 1 -r 2 -p 1 -g $size &> times/EXACT
-time ./ClusterHL -S 1 -r 3 -p 2 -g $size &> times/MC
-time ./ClusterDLA -s 1 -r 0 -c /dev/null -p $size &> times/DLA
-time ./ClusterDLA -s 1 -r 1 -c /dev/null -p $size &> times/DLAMC
+{ echo "HL "$size ; time ./ClusterHL -S 1 -r 1 -p 2 -g $size ; } &>> times/HL
+{ echo "EXACT "$size ; time ./ClusterHL -S 1 -r 2 -p 1 -g $size ; } &>> times/EXACT
+{ echo "MC "$size ; time ./ClusterHL -S 1 -r 3 -p 2 -g $size ; } &>> times/MC
+{ echo "DLA "$size ; time ./ClusterDLA -s 1 -r 0 -c /dev/null -p $size ; } &>> times/DLA
+{ echo "DLAMC "$size ; time ./ClusterDLA -s 1 -r 1 -c /dev/null -p $size ; } &>> times/DLAMC
