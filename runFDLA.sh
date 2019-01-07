8c98102df7aaec2f509daa599002204ea7b81526
James Bell <jbell@posteo.net>
2019-01-07 16:36:33 +0000

    started writing scripts for FDLA

---
+++ runFDLA.sh
@@ -0,0 +1,8 @@
+top=$(($1-1))
+i=1
+while [ "$i" -le "$3" ]; do
+  bottom=$(($top+1))
+  top=$(($1+(($2-$1)*$i)/$3))
+  time nohup nice -19 taskset -c 0-140 FDLAClusters.sh $bottom $top 100000000 &
+  i=$(($i+1))
+done
