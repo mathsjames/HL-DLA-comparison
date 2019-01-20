3d574a2ba45b856d465780603677da6e5c0dfd8d
mathsjames <james.bell1@hotmail.com>
2019-01-20 15:59:53 +0000

    Improved summary presentation

--- src/other/summarize.c
+++ src/other/summarize.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define models 8
+
 double pvalue(double energies[100000])
 {
   int tot=0;
@@ -22,32 +24,68 @@ double mean(double energies[100000])
 
 int main()
 {
-  char* fprefixes[6];
+  char* fprefixes[models];
   fprefixes[0]="P2HLN100000S";
   fprefixes[1]="P1EXACTN100000S";
   fprefixes[2]="P2MCN100000S";
   fprefixes[3]="DLAMC0N";
   fprefixes[4]="DLAMC1N";
   fprefixes[5]="FDLA";
+  fprefixes[6]="NR5";
+  fprefixes[7]="NR7";
+  double sizes[models]={3000,500,500,16000,5000,3000,5000,3000};
   char filename[100];
   double energies[100000];
   FILE* fp;
-  for (int i=0;i<6;i++)
+  double output[3][models][models];
+  for (int i=0;i<models;i++)
     {
-      for (int j=i+1;j<6;j++)
+      for (int j=i+1;j<models;j++)
 	{
 	  sprintf(filename,"energies/energies%s-%s",fprefixes[i],fprefixes[j]);
 	  if (fp=fopen(filename,"r"))
 	    {
 	      fread(energies,sizeof(double),100000,fp);
-	      printf("Report for %s-%s\nenergy is %lf\np-value is %lf\navg permutation energy is %lf\n\n",fprefixes[i],fprefixes[j],energies[0],pvalue(energies),mean(energies));
+	      output[0][i][j]=energies[0];
+	      output[1][i][j]=pvalue(energies);
+	      output[2][i][j]=energies[0]*(sizes[i]+sizes[j])/(sizes[i]*sizes[j]);
+	      printf("Report for %s-%s\nenergy is %lf\np-value is %lf\ndistance is %lf\navg permutation energy is %lf\n\n",fprefixes[i],fprefixes[j],output[0][i][j],output[1][i][j],output[2][i][j],mean(energies));
+	      for (int o=0;o<3;o++) {
+		output[o][j][i]=output[o][i][j];
+	      }
 	    }
 	  else
 	    {
-	      printf("Failed to open %s",filename);
+	      printf("Failed to open %s\n",filename);
 	    }
 	}
     }
+  int indicies[models]={3,4,0,1,2,5,6,7};
+  char* outputs[3];
+  outputs[0]="Energies";
+  outputs[1]="p-values";
+  outputs[2]="differences";
+  for (int o=0;o<3;o++) {
+    printf("%s\n",outputs[o]);
+    for (int i=0;i<models;i++)
+      {
+	int k=indicies[i];
+	for (int j=0;j<models;j++)
+	  {
+	    int l=indicies[j];
+	    if (i<j)
+	      {
+		printf("%-10lf",output[o][k][l]);
+	      }
+	    else
+	      {
+		printf("          ");
+	      }
+	  }
+	printf("\n");
+      }
+    printf("\n");
+  }
   return 0;
 }
   
